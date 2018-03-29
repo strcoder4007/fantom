a071ff96909064bd98bb1c7296507e32ede5f130
Shubham Singh <mynameshubham@gmail.com>
2018-03-29 05:35:20 +0400

    reviews parsed

--- app.js
+++ app.js
@@ -5,11 +5,22 @@ const phantom = require('phantom');
     const page = await instance.createPage();
     const status = await page.open('http://www.tigerdirect.com/applications/SearchTools/item-details.asp?EdpNo=3415697');
     const content = await page.property('content');
-
-    var chunk = await page.evaluate(function() {
-        return document.querySelector('.reviewer').innerHTML;
+    
+    var reviews = await page.evaluate(function() {
+        var junk = [];
+        var xs = document.querySelectorAll('.reviewer');
+        for(var i = 0; i < xs.length; i++) {
+            var myObj = {
+                reviewer: xs[i].querySelectorAll('dd')[0].innerHTML,
+                date: xs[i].querySelectorAll('dd')[1].innerHTML
+            }
+            junk.push(myObj);
+        }
+        return junk;
     });
-    console.log(chunk.split('\n')[1]);
+
+    for(let i = 0; i < reviews.length; i++)
+        console.log(reviews[i].reviewer + " " + " " + reviews[i].date);
 
     await instance.exit();
 }());
